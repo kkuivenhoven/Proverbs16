0d0d13ff1886727d673bcf2401aca106c65e6648
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2019-03-13 16:00:30 -0700

    (api): added more to api layer

--- app/controllers/api/v1/niv_bible_controller.rb
+++ app/controllers/api/v1/niv_bible_controller.rb
@@ -1,12 +1,22 @@
 class Api::V1::NivBibleController < ApplicationController
 
   def index
+		@chapter = Hash.new
+		@verses = Hash.new
 		if !params[:chapNum].nil?
-			@proverbs = Proverb.where("chapter = #{params[:chapNum]}").all.pluck(:verse_text)
+			@proverbs = Proverb.where("chapter = #{params[:chapNum]}").all.pluck(:verse_num, :verse_text).to_h
+			@common_sw = CommonWord.where("chapter_num = #{params[:chapNum]}").all.pluck(:verse_num_sw)
+			@verses["verses"] = @proverbs
+			@verses["stop_worded_verses"] = @common_sw
+			@chapter[params[:chapNum]] = @verses
 		else
-			@proverbs = Proverb.where("chapter = '1'").all.pluck(:verse_text)
+			@proverbs = Proverb.where("chapter = '1'").all.pluck(:verse_num, :verse_text).to_h
+			@common_sw = CommonWord.where("chapter_num = '1'").all.pluck(:verse_num_sw)
+			@verses["verses"] = @proverbs
+			@verses["stop_worded_verses"] = @common_sw
+			@chapter[1] = @verses
 		end
-		render json: @proverbs
+		render json: @chapter
 	end
 
 end
