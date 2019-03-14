894b55f3186fb7ddfc13277921e17505bd74d1c1
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2019-03-13 20:48:53 -0700

    (API): added another field to each JSON entry for API layer

--- app/controllers/api/v1/niv_bible_controller.rb
+++ app/controllers/api/v1/niv_bible_controller.rb
@@ -5,9 +5,11 @@ class Api::V1::NivBibleController < ApplicationController
 		if !params[:chapNum].nil?
 			@verses = Hash.new
 			@proverbs = Proverb.where("chapter = #{params[:chapNum]}").all.pluck(:verse_num, :verse_text).to_h
+			@rec_nsw = CommonWord.where("chapter_num = #{params[:chapNum]}").all
 			@common_sw = CommonWord.where("chapter_num = #{params[:chapNum]}").all.pluck(:verse_num_sw)
 			@ch_common_sw = CommonWord.getCommonCount(@common_sw)
 			@verses["verses"] = @proverbs
+			@verses["rec_nsw"] = @rec_nsw
 			@verses["stop_worded_verses"] = @ch_common_sw
 			@chapter[params[:chapNum]] = @verses
 		else
@@ -15,9 +17,11 @@ class Api::V1::NivBibleController < ApplicationController
 			chapters.each do |c|
 				@verses = Hash.new
 				@proverbs = Proverb.where("chapter = #{c}").all.pluck(:verse_num, :verse_text).to_h
+				@rec_nsw = CommonWord.where("chapter_num = #{params[:chapNum]}").all
 				@common_sw = CommonWord.where("chapter_num = #{c}").all.pluck(:verse_num_sw)
 				@ch_common_sw = CommonWord.getCommonCount(@common_sw)
 				@verses["verses"] = @proverbs
+				@verses["rec_nsw"] = @rec_nsw
 				@verses["stop_worded_verses"] = @ch_common_sw
 				@chapter[c] = @verses
 			end
