92c58cc025e63d6cf76e3f7462221917cedcbef5
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2019-03-16 15:24:33 -0700

    (refactor): removed unused common controller and associated routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,6 @@ Rails.application.routes.draw do
   get '/google0d522795c8df8607.html',
     to: proc { |env| [200, {}, ["google-site-verification: google0d522795c8df8607.html"]] }
 
-  get 'common/words'
-  get 'common/totalCount'
-  get 'common/plot'
-
 	root controller: :static_pages, action: :home
 
 end
